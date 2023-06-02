12ff62267a141a93cf2659f0302eadba7dcf0a5c
fat-fella <aznclee711@gmail.com>
2023-06-02 17:39:12 +0900

    IO,Exception 수업
    merong

--- 20230526_java/src/kh/lclass/io/TestFileIO.java
+++ 20230526_java/src/kh/lclass/io/TestFileIO.java
@@ -1,5 +1,36 @@
 package kh.lclass.io;
 
-public class TestFileIO {
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
+public class TestFileIO {
+	public void testFile() /* throws IOException */ {
+//		File f1 = new File("D:/data1/aaa.txt");
+//		f1.mkdirs();
+		String path = "D:/data2/test";
+		String filename = "aaa.txt";
+		FileWriter fw = null;
+
+		new File(path).mkdirs();
+		File f2 = new File(path, filename);
+
+		try {
+			f2.createNewFile();
+
+			fw = new FileWriter(f2);
+			fw.write("abcdefg");
+			fw.flush();
+			return;
+		} catch (IOException e) {
+			System.out.println("cannot create file");
+			e.printStackTrace();
+		} finally {
+			try {
+				if (fw != null) fw.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }
--- 20230526_java/src/kh/lclass/oop/sample/Kh.java
+++ 20230526_java/src/kh/lclass/oop/sample/Kh.java
@@ -1,10 +1,47 @@
 package kh.lclass.oop.sample;
 
+import java.awt.AWTEvent;
+import java.awt.Rectangle;
+import java.awt.im.spi.InputMethodContext;
 import java.io.Serializable;
+import java.lang.Character.Subset;
+import java.util.Locale;
 
 @SuppressWarnings("serial")
 public class Kh implements TestInterface, Serializable{
 	public void khSpecial() {
+		String str = "1111122221111122222222222222";
+		try {
+			Car[] carArr = new Car[3];
+			carArr[4].setPrice(5000);
+			carArr[0].setPrice(5000); //NullPointerException
+			
+			int a = Integer.parseInt(str);
+			
+		} catch (NullPointerException e) {
+			System.out.println("merong");
+			e.printStackTrace();
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("merong~");
+			e.printStackTrace();
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		
 	}
 	@Override
@@ -23,6 +60,86 @@ public class Kh implements TestInterface, Serializable{
 	public int method3(int a, int b) {
 		// TODO Auto-generated method stub
 		return 0;
+	}
+	@Override
+	public void setInputMethodContext(InputMethodContext context) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public boolean setLocale(Locale locale) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public Locale getLocale() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public void setCharacterSubsets(Subset[] subsets) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void setCompositionEnabled(boolean enable) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public boolean isCompositionEnabled() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public void reconvert() {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void dispatchEvent(AWTEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void notifyClientWindowChange(Rectangle bounds) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void activate() {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void deactivate(boolean isTemporary) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void hideWindows() {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void removeNotify() {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void endComposition() {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public Object getControlObject() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 	
 }
