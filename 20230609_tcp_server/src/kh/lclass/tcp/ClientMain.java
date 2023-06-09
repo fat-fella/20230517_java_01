1c046dc9042941d3431bacd18d1cdd6fdc8c1cf0
fat-fella <aznclee711@gmail.com>
2023-06-09 17:52:53 +0900

    tcpserver

---
+++ 20230609_tcp_server/src/kh/lclass/tcp/ClientMain.java
@@ -0,0 +1,6 @@
+package kh.lclass.tcp;
+
+public class ClientMain {
+	public static void main(String[] args) {
+	new TcpClient().testTcpClient("127.0.0.1", 9001);
+}}
---
+++ 20230609_tcp_server/src/kh/lclass/tcp/Server.java
@@ -0,0 +1,9 @@
+package kh.lclass.tcp;
+
+public class Server {
+
+	public static void main(String[] args) {
+		new TcpServer().testTcpServer(9001);// 1.서버의 포트 번호 정함
+	}
+
+}
